84f6294fef33cd0f3d211e86a74d9b4354b05a8c
Caroline Kranefuss <98840718+caroline-kranefuss@users.noreply.github.com>
2025-11-13 16:20:32 -0500

    Update sources with new URLs and descriptions

--- sources.py
+++ sources.py
@@ -1,4 +1,7 @@
 """
-http://www.fws.gov/sites/default/files/documents/2024-08/percent-forest-cover-by-county-update-2024.xlsx
-https://en.wikipedia.org/wiki/2024_United_States_wildfires
+http://www.fws.gov/sites/default/files/documents/2024-08/percent-forest-cover-by-county-update-2024.xlsx Forest cover in 2024
+https://en.wikipedia.org/wiki/2024_United_States_wildfires Wildfires in 2024
+https://www.aafa.org/wp-content/uploads/2022/10/aafa-2022-asthma-capitals-report.pdf Asthma death rate and ER visits in 2022 
+https://aafa.org/wp-content/uploads/2023/09/aafa-2023-asthma-capitals-report.pdf Asthma death rate and ER visits in 2023 
+https://aafa.org/wp-content/uploads/2025/09/aafa-2025-asthma-capitals-report.pdf Asthma death rate and ER visits in 2025
 """
