5f1a6b28725e029f80a92fd7a7320465527d9dfe
Caroline Kranefuss <98840718+caroline-kranefuss@users.noreply.github.com>
2025-11-13 18:17:46 -0500

    Add link for counties in metropolitan areas

--- sources.py
+++ sources.py
@@ -4,4 +4,5 @@ https://en.wikipedia.org/wiki/2024_United_States_wildfires Wildfires in 2024
 https://www.aafa.org/wp-content/uploads/2022/10/aafa-2022-asthma-capitals-report.pdf Asthma death rate and ER visits in 2022 
 https://aafa.org/wp-content/uploads/2023/09/aafa-2023-asthma-capitals-report.pdf Asthma death rate and ER visits in 2023 
 https://aafa.org/wp-content/uploads/2025/09/aafa-2025-asthma-capitals-report.pdf Asthma death rate and ER visits in 2025
+https://www.whitehouse.gov/wp-content/uploads/2023/07/OMB-Bulletin-23-01.pdf Counties for metropolitan areas in the US
 """
